1335ff5ae0268533f867aef46a2ea02260007e0d
Brent Reynolds <brentsreynolds5@gmail.com>
2023-08-18 10:34:21 -0400

    Made function stubs.

--- wordle.py
+++ wordle.py
@@ -1,9 +1,26 @@
 import random
 
-list_of_words = []
-def word_list():
+def word_list(list_of_words):
     with open('5_letter_words.txt', 'r') as file:
         for line in file:
            line = line.strip()
            list_of_words.append(line)
     return list_of_words
+
+def random_word(list_of_words):
+    pass
+
+def is_real_word(guess, list_of_words):
+    pass
+
+def check_guess(word_entered, actual_word):
+    pass
+
+def next_guess(list_of_words):
+    pass
+
+def main():
+    list_of_words = []
+    print(word_list(list_of_words))
+
+main()
